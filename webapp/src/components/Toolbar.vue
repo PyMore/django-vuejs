eaf7b7fdf8c7adf57ebe2dc44c3587112e61ef56
CarlsKe <fernancarldiaz@outlook.com>
2018-05-11 00:56:00 -0500

    feat: added form skeleton

--- webapp/src/components/Toolbar.vue
+++ webapp/src/components/Toolbar.vue
@@ -1,19 +1,13 @@
 <template>
     <div>
         <md-toolbar class="md-large md-primary">
-
             <div class="md-toolbar-row">
-                <div class="md-toolbar-section-start">
-                    <md-button class="md-icon-button">
-                        <md-icon>menu</md-icon>
-                    </md-button>
+
+               <div class="md-toolbar-row md-toolbar-offset">
+                    <h3 class="md-title">Are you ready?</h3>
                 </div>
 
                 <div class="md-toolbar-section-end">
-                    <md-button class="md-icon-button">
-                        <md-icon>refresh</md-icon>
-                    </md-button>
-
                     <md-button class="md-icon-button">
                         <md-icon>more_vert</md-icon>
                     </md-button>
@@ -21,11 +15,77 @@
 
             </div>
 
-            <div class="md-toolbar-row md-toolbar-offset">
-                <h3 class="md-title">Are you ready?</h3>
+        </md-toolbar>        
+
+        <md-tabs class="md-primary" md-alignment="fixed">
+            <md-tab id="tab-home" md-icon="home">
+            </md-tab>
+            <md-tab id="tab-pages" md-icon="pages">
+            </md-tab>
+            <md-tab id="tab-favorites" md-icon="favorite"></md-tab>
+            <md-tab id="tab-favorites4" md-icon="favorite"></md-tab>
+
+        </md-tabs>
+
+
+    <div class="section">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Card without hover effect</div>
+      </md-card-header>
+
+      <md-card-content>
+        <form action="">
+            
+            <md-datepicker v-model="selectedDate" />
+            </md-field>
+
+            <md-field>
+            <label>Hours</label>
+            <md-input v-model="number" type="number"></md-input>
+            </md-field>
+
+            <md-field>
+            <label>Comment</label>
+            <md-textarea v-model="autogrow" md-autogrow></md-textarea>
+            </md-field>
+
+
+            <div class="md-layout-item">
+                <md-field>
+                <label for="font">Font</label>
+                <md-select v-model="font" name="font" id="font" md-dense>
+                    <md-option value="arial">Arial</md-option>
+                    <md-option value="calibri">Calibri</md-option>
+                    <md-option value="cambria">Cambria</md-option>
+                    <md-option value="comic-sans">Comic Sans</md-option>
+                    <md-option value="consolas">Consolas</md-option>
+                    <md-option value="courier">Courier</md-option>
+                    <md-option value="droid-sans">Droid Sans</md-option>
+                    <md-option value="georgia">Georgia</md-option>
+                    <md-option value="helvetica">Helvetica</md-option>
+                    <md-option value="impact">Impact</md-option>
+                    <md-option value="roboto">Roboto</md-option>
+                    <md-option value="segoe-ui">Segoe UI</md-option>
+                    <md-option value="times-new-roman">Times New Roman</md-option>
+                    <md-option value="ubuntu">Ubuntu</md-option>
+                    <md-option value="verdana">Verdana</md-option>
+                </md-select>
+                </md-field>
             </div>
 
-        </md-toolbar>        
+
+            <md-button type="submit" class="md-primary md-raised">Open Snackbar</md-button>
+
+        </form>        
+      </md-card-content>
+
+    </md-card>
+
+    </div>
+
+
+
     </div>
 </template>
 
@@ -33,8 +93,16 @@
     
 </script>
 
-<style scoped>
-md-toolbar{
-    background-color: blue;
-}
+
+<style  scoped>
+    md-toolbar{
+        background-color: blue;
+    }
+
+
+  .section {
+    margin-top: 3em;
+    margin-left: 25%;
+    max-width: 50%;
+  }
 </style>
